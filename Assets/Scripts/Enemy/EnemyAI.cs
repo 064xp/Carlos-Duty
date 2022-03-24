eb7df369b057b80030272bbcd31e33d4902ec6cd
Paulo <paulozamora2000@gmail.com>
2022-03-24 00:01:20 -0600

    Basic setup for Enemy AI state machine

---
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private enum States {
+        GoToTarget,
+        AttackPlayer,
+        BreakBarricade,
+        AttackTarget,
+        EnterBuilding,
+        Die
+    };
+
+    private NavMeshAgent agent;
+    [SerializeField]
+    private States state = States.GoToTarget;
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Transform player;
+
+    Vector3 directionToPlayer;
+    bool inSight = false;
+
+    public float shootDistance = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateStates();
+        CheckForPlayer();
+    }
+
+    private void UpdateStates() {
+        switch (state) {
+            case States.GoToTarget:
+                GoToTarget();
+                break;
+            case States.AttackPlayer:
+                AttackPlayer();
+                break;
+            case States.BreakBarricade:
+                break;
+            case States.AttackTarget:
+                AttackTarget();
+                break;
+            case States.EnterBuilding:
+                break;
+            case States.Die:
+                Die();
+                break;
+        }
+    }
+
+    void CheckForPlayer() {
+        directionToPlayer = player.position - transform.position;
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, directionToPlayer.normalized, out hit)) {
+            inSight = hit.transform.CompareTag("Player");
+        }
+    }
+
+    private void GoToTarget() {
+        if(agent.destination != target.position)
+            agent.SetDestination(target.position);
+
+        if(directionToPlayer.magnitude <= shootDistance && inSight) {
+            agent.ResetPath();
+            print("attack player state");
+            state = States.AttackPlayer;
+        }
+
+        if (HasReached()) {
+            state = States.AttackTarget;
+            print("Attack target state");
+        }
+        //if (agent.destination != target.position) {
+        //}
+    }
+
+    private void AttackPlayer() {
+        if (agent.destination != player.transform.position)
+           agent.SetDestination(player.transform.position);
+
+        if(inSight) {
+            LookAt(player);
+            print("Fire!");
+        }
+    }
+
+    private void AttackTarget() {
+
+    }
+
+    private void Die() {
+        // play death animation
+        Destroy(this.gameObject, 5);
+    }
+
+    // Utility functions
+
+    private void LookAt(Transform target) {
+        Vector3 lookDirection = target.position - transform.position;
+
+        Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * agent.angularSpeed);
+    }
+
+    private bool HasReached() {
+        return agent.hasPath && !agent.pathPending && agent.remainingDistance <= agent.stoppingDistance;
+    }
+}
